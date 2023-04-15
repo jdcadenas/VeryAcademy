d5f202d20ff57caf99b12d94bf70b333efff5f23
José Daniel Cadenas <jose.cadenas@pmsonline.com.ve>
2023-04-15 16:45:46 -0400

    init commit

--- drfecommerce/settings.py
+++ drfecommerce/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)=m8g5%t84+q^lz3-ft+bw&jw+zof2-r^-fo2zwxqn-_a$f2i0'
+load_dotenv()
+
+SECRET_KEY= os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External packages
+    'rest_framework',
+    #Internal Apps
+    
 ]
 
 MIDDLEWARE = [
