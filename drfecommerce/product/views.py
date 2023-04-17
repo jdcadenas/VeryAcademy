3b2d714a124a9e46b5388a6807e7e16977a2449c
José Daniel Cadenas <jose.cadenas@pmsonline.com.ve>
2023-04-16 20:24:37 -0400

    api swagger UI 18

---
+++ drfecommerce/product/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+from drf_spectacular.utils import extend_schema
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    a simple viewset for viewing all categories
+    """
+
+    queryset = Category.objects.all()
+    
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
--- drfecommerce/settings.py
+++ drfecommerce/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External packages
     "rest_framework",
+    "drf_spectacular",
     # Internal Apps
+    "drfecommerce.product",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+    # OTHER SETTINGS
+}
